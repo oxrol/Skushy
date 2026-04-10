be1ce3d9f6cf85bd9a43e0940781b9eeb94214fd
oxrol <mehmetefetuzcu@gmail.com>
2026-04-10 23:12:31 +0200

    Add files via upload

---
+++ skushy-site/api/images.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  const imagesDir = path.join(process.cwd(), 'images');
+
+  try {
+    const files = fs.readdirSync(imagesDir).filter(f =>
+      /\.(png|jpg|jpeg|webp)$/i.test(f)
+    );
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({ images: files });
+  } catch (e) {
+    res.json({ images: [] });
+  }
+};
