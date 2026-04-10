01d7db359add55c4d0288a1d54bd64c375abab1a
oxrol <mehmetefetuzcu@gmail.com>
2026-04-10 23:17:22 +0200

    Update images.js

--- skushy-site/api/images.js
+++ skushy-site/api/images.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-
+ 
 module.exports = (req, res) => {
-  const imagesDir = path.join(process.cwd(), 'images');
-
+  const imagesDir = path.join(process.cwd(), 'skushy-site', 'images');
+ 
   try {
     const files = fs.readdirSync(imagesDir).filter(f =>
       /\.(png|jpg|jpeg|webp)$/i.test(f)
@@ -11,6 +11,8 @@ module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.json({ images: files });
   } catch (e) {
-    res.json({ images: [] });
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({ images: [], error: e.message });
   }
 };
+ 
